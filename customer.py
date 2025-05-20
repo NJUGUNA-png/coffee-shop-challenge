a33dc145986b9b1a3b4182e1ce7c4371d194c8e6
Njuguna <engbrian01@gmail.com>
2025-05-20 08:31:19 +0300

    added customer class with mutable name property and implemented name getter with proper validation

---
+++ customer.py
@@ -0,0 +1,19 @@
+class Customer:
+    def _init_(self, name):
+        self._name = None
+        self.name = name  
+        self._orders = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        if not (1 <= len(value) <= 15):
+            raise ValueError("Name must be between 1 and 15 characters")
+        self._name = value
+
+    
