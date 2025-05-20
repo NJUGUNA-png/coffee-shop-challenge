de5afad81ae1f5bbf6c298dc750c8dea95cee4f9
Njuguna <engbrian01@gmail.com>
2025-05-20 08:23:37 +0300

    added coffee class with name validation (a string,minimum 3 characters)

--- coffee.py
+++ coffee.py
@@ -1,1 +1,11 @@
-class 
+class Coffee:
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        if len(name) < 3:
+            raise ValueError("Name must be at least 3 characters")
+            
+        self._name = name
+        self._orders = []
+
+    
