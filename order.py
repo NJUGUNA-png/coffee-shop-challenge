d6472295f189dcef46dc799474dd5227761855d7
Njuguna <engbrian01@gmail.com>
2025-05-20 08:42:26 +0300

    Ensured orders are automatically added to customer and coffee records

--- order.py
+++ order.py
@@ -16,4 +16,22 @@ class Order:
         if not (1.0 <= float(price) <= 10.0):
             raise ValueError("Price must be between 1.0 and 10.0")
             
+        self._customer = customer
+        self._coffee = coffee
+        self._price = float(price) 
         
+        
+        customer._orders.append(self)
+        coffee._orders.append(self)
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @property
+    def price(self):
+        return self._price
